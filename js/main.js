7ed2c5af5420f04013c309412c508c9cc10a0cac
jamesq68 <jamesq68@yahoo.com>
2015-02-07 12:36:45 -0500

    initial commit
    Add final project files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.hamburger').on('click', function () {
+	$('.navigation').slideToggle();
+});
+
+// Flexslider code
+// $(window).load(function() {
+//   $('.flexslider').flexslider({
+//     animation: "slide",
+//     controlNav: "thumbnails"
+//   });
+// });
